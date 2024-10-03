3e6caebde4a4384e36bf3700f2248b771044f2a8
Adithyan mohan <78484469+Adithyanmohanadz@users.noreply.github.com>
2024-10-03 14:11:47 +0530

    test

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -216,6 +216,40 @@
                         </a>
                       </td>
                     </tr>
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img src="assets/img/product.jpg" class="avatar avatar-sm me-3" alt="user3">
+                          </div>
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">Product name</h6>
+                            <p class="text-xs text-secondary mb-0">Product id number</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p class="text-xs font-weight-bold mb-0">user name</p>
+                      </td>
+                      <td>
+                        <p class="text-xs font-weight-bold mb-0">+91 000 000 0000</p>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="badge badge-sm bg-gradient-danger">pending</span>
+                      </td>
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">19/09/17</span>
+                      </td>
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">25/10/17</span>
+                      </td>
+                      <td class="align-middle">
+                        <a href="product.html" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
+                          data-original-title="Edit user">
+                          Edit
+                        </a>
+                      </td>
+                    </tr>
                   </tbody>
                 </table>
               </div>
